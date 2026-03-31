794742bf1be6fb0e88d855f4d4923ad8b5dd2139
adxnis21 <jereado2107@gmail.com>
2026-03-31 23:18:35 +0200

    Update cache version from v9 to v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gympro-v9';
+const CACHE = 'gympro-v10';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './data.js', './manifest.json'];
 
 self.addEventListener('install', e => {
