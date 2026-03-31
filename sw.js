0a53ab9172ed40ea9ee128cd48e0a2af04e54c63
adxnis21 <jereado2107@gmail.com>
2026-03-31 19:31:54 +0200

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gympro-v3';
+const CACHE = 'gympro-v4';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './data.js', './manifest.json'];
 
 self.addEventListener('install', e => {
