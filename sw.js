ca501cb02414180aafb97da06bf9bc5803b6c36f
adxnis21 <jereado2107@gmail.com>
2026-04-03 18:01:14 +0200

    Update cache version to 'gympro-v21'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gympro-v20';
+const CACHE = 'gympro-v21';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './data.js', './manifest.json'];
 
 self.addEventListener('install', e => {
